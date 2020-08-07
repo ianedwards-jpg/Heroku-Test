ab532540e2b0ea7ac22d508d821a9979e541c1a1
Ian Edwards <ian.edward@uconn.edu>
2020-08-07 18:22:51 -0400

    Update
    Ran npm build

---
+++ client/build/precache-manifest.7f5a44a7826fcd96203a693380eb61a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db82ea8b82750522cc8c",
+    "url": "/static/css/main.75faa611.chunk.css"
+  },
+  {
+    "revision": "db82ea8b82750522cc8c",
+    "url": "/static/js/main.5a52c81b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "fec8d1367609827a70f375c9b6507fc9",
+    "url": "/index.html"
+  }
+];
